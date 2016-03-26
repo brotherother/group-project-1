06851888deec787d8c4ab9de75791853c1aa1311
brotherother <dennis.r.flynn@gmail.com>
2016-03-26 13:45:16 -0400

    Saturday#2

--- assets/javascript/sqoot.js
+++ assets/javascript/sqoot.js
@@ -1,29 +1,36 @@
 // On hitting the search submit button, data fields are loaded into 
-$('#search-submit').on('click'), function() {
-	var local = $('#addressInput').val.trim() + $('#cityInput').val.trim() + $('#stateInput').val.trim() + $('#zipInput').val.trim()
-	var radius = $('#radius').val.trim();
-	var numberOfResults = $('#numberOfResults').val.trim();
+$(document).ready(function(){
+	$('#searchButton').click(function() {
+		var location = $('#addressInput').val().trim() + $('#cityInput').val().trim() + $('#stateInput').val().trim() + $('#zipInput').val().trim()
+		var radius = $('#radiusInput').val().trim();
+		var numberOfResults = $('#resultsNumberInput').val().trim();
+		console.log(location);
 
-	console.log(local);
-	renderResults(location, radius, nubmer);
-};
+		renderResults(location, radius, numberOfResults);
+	});
 
-function renderResults(location, radius, number) {
+
+});
+
+
+function renderResults(location, radius, numberOfResults) {
 	$('#resultsView').empty();
 
-	var queryURL = "http://api.sqoot.com/v2/coupons?location=" + location  + "&radius=" + radius + "&per_page=" + number + "&api_key=dlmxb4";
+	var queryURL = "http://api.sqoot.com/v2/coupons?location=" + location  + "&radius=" + radius + "&per_page=" + numberOfResults + "&api_key=dlmxb4";
 
+	console.log(queryURL);
 	console.log(results.coupons);
 
 	$.ajax({url: queryURL, method: 'GET'}).done(function(response) {
 		var results = response.coupons;
-		console.log(respo.coupons);
+		console.log(response.coupons);
 		for (var i = 0; i < numberOfResults; i++) {
 			var a = $('<button>');
-			a.id('couponResult' + i);
+			a.attr('id', 'couponResult' + i);
 			a.addClass('coupon-result');
-			a.attr('deal-description', results.coupons[i].description);
-			a.attr('address', results.coupons[i].address);
+			a.attr('deal-description', results[i].coupon.description);
+			console.log(results[i].coupon.description);
+			a.attr('address', results[i].coupon.address);
 			a.attr('chosen-status');
 			$('#results').prepend(a);
 		};
