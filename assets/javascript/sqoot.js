087928d7771b5a0841c6e63be1cad22cfb9e499e
brotherother <dennis.r.flynn@gmail.com>
2016-03-31 17:41:26 -0400

    working buttons v2

--- assets/javascript/sqoot.js
+++ assets/javascript/sqoot.js
@@ -1,45 +1,48 @@
 // On hitting the search submit button, data fields are loaded into 
 $(document).ready(function(){
+
 	$('#searchButton').click(function() {
 		var location = $('#addressInput').val().trim() + $('#cityInput').val().trim() + $('#stateInput').val().trim() + $('#zipInput').val().trim()
-		console.log(location);
-		debugger;
 		location = encodeURIComponent(location);
-		console.log(location);
-		debugger;
 		var radius = $('#radiusInput').val().trim();
-		debugger;
-		console.log(radius);
-		debugger;
 		// var numberOfResults = $('#resultsNumberInput').val().trim();
-		renderResults(location, radius, numberOfResults);
+		renderResults(location, radius);
 	});
 
 
 });
 
 
-function renderResults(location, radius, numberOfResults) {
+function renderResults(location, radius) {
 	$('#resultsView').empty();
 
-	var queryURL = "http://api.sqoot.com/v2/coupons?location=" + location  + "&radius=" + radius + "&per_page=" + numberOfResults + "&api_key=dlmxb4";
+	var queryURL = "http://api.sqoot.com/v2/coupons?location=" + location  + "&radius=" + radius + "&api_key=dlmxb4";
 
 	console.log(queryURL);
-	console.log(results.coupons);
+	// console.log(results.coupons);
 
-	$.ajax({url: queryURL, method: 'GET'}).done(function(response) {
-		var results = response.coupons;
-		console.log(response.coupons);
-		for (var i = 0; i < numberOfResults; i++) {
-			var a = $('<button>');
-			a.attr('id', 'couponResult' + i);
-			a.addClass('coupon-result');
-			a.attr('deal-description', results[i].coupon.description);
-			console.log(results[i].coupon.description);
-			a.attr('address', results[i].coupon.address);
-			a.attr('chosen-status');
-			$('#results').prepend(a);
-		};
+	$.ajax({
+		url: queryURL, 
+		dataType: 'jsonp', 
+		method: 'GET',
+		success: function(data) {
+			var results = data.coupons;
+			console.log(data.coupons);
+			for (var i = 0; i < results.length; i++) {
+				$('#resultsView').append('<button>' + results[i].coupon.title + '</button>');
+
+
+					
+				// var a = $('<button>');
+				// a.attr('id', 'couponResult' + i);
+				// a.addClass('coupon-result');
+				// a.attr('deal-description', results[i].coupon.description);
+				// console.log(results[i].coupon.description);
+				// a.attr('address', results[i].coupon.address);
+				// a.attr('chosen-status');
+				// $('#results').prepend(a);
+			};
+		}
 	});
 };
 
